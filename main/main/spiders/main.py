217704944d0f86dc8e64f43870df044998d371b0
Afaq Ahmad <afaq.ahmad@arbisoft.com>
2020-08-10 13:37:41 +0500

    main.py cleaned of comments

--- main/main/spiders/main.py
+++ main/main/spiders/main.py
@@ -18,20 +18,12 @@ class QuotesSpider(scrapy.Spider):
             print(next_i)
             yield scrapy.Request(response.urljoin(str(next_i[0])), callback=self.parse_data)
 
-
     def parse_data(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'task3-%s.html' % page
         result = response.xpath('//script[contains(.,"title")]/text()').extract()
         title = re.compile('"title":"(.*?)"')
-        locality = re.compile('"addressLocality":"(.*?)"')
-        region = re.compile('"addressRegion":"(.*?)"')
-        country = re.compile('"addressCountry":"(.*?)"')
         comp_name = re.compile('"name":"(.*?)"')
-        logo = re.compile('"logo":"(.*?)"')
+        # logo = re.compile('"logo":"(.*?)"')
         skills = re.compile('"skills":\[(.*?)]')
-        benefits = re.compile('"jobBenefits":\[(.*?)]')
-        about = re.compile('"description":"<h2>(.*?)<h2>')
 
         yield {
             'Job title': title.search(result[0]).group(1),
@@ -48,19 +40,15 @@ class QuotesSpider(scrapy.Spider):
                 '''normalize-space(//header[@class="job-details--header grid mb24 gs12 gsx sm:fd-column sm:ta-center"]
                 //span[@class="fc-black-500"]/text())'''
                 ).get(),
-              #str(locality.search(result[0]).group(1))
-               #             + ", " + str(region.search(result[0]).group(1))
-                #            + ", " + str(country.search(result[0]).group(1)),
+
             'Skills Required': skills.search(result[0]).group(1),
             'Perks offered': response.xpath(
                 '//section[contains(., "Benefits")]/ul//span/text()'
                 ).extract(),
-                #benefits.search(result[0]).group(1),
 
             'About This Job': response.xpath(
                 '//section[contains(., "About this job")]//span/text()'
                 ).extract(),
-                #about.search(result[0]).group(1),
 
             'Description': ''.join(response.xpath(
                 '//section[contains(., "Job description")]/div/ul//text()'
