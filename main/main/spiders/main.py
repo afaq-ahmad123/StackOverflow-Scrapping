269c7bb5a5d4d2e23c9296f0ce1f6239a90b7ccd
Afaq Ahmad <afaq.ahmad@arbisoft.com>
2020-08-10 11:18:16 +0500

    xpaths edited

--- main/main/spiders/main.py
+++ main/main/spiders/main.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class QuotesSpider(scrapy.Spider):
@@ -8,48 +9,63 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def __init__(self, skill='blockchain', location='Boston USA', *args, **kwargs):
-
         self.start_urls.append(f'https://stackoverflow.com/jobs?q={skill}&l={location}&d=20&u=Km')
         super().__init__(*args, **kwargs)
 
     def parse(self, response):
-
         for res in response.xpath('//a[@class = "s-link stretched-link"]'):
             next_i = res.xpath('@href').extract()
             print(next_i)
             yield scrapy.Request(response.urljoin(str(next_i[0])), callback=self.parse_data)
-            break
+
 
     def parse_data(self, response):
         page = response.url.split("/")[-2]
         filename = 'task3-%s.html' % page
         result = response.xpath('//script[contains(.,"title")]/text()').extract()
-        yield {
-            'title': result[0]
-        }
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        #
+        title = re.compile('"title":"(.*?)"')
+        locality = re.compile('"addressLocality":"(.*?)"')
+        region = re.compile('"addressRegion":"(.*?)"')
+        country = re.compile('"addressCountry":"(.*?)"')
+        comp_name = re.compile('"name":"(.*?)"')
+        logo = re.compile('"logo":"(.*?)"')
+        skills = re.compile('"skills":\[(.*?)]')
+        benefits = re.compile('"jobBenefits":\[(.*?)]')
+        about = re.compile('"description":"<h2>(.*?)<h2>')
 
-        # for quote in response.css('div.quote'):
-        #     yield {
-        #         # 'Job':
-        #         # 'Title':
-        #         # 'Company':
-        #         # 'Company':
-        #         # 'logo’s':
-        #         # 'image_url':
-        #         # 'Location':
-        #         # 'Skills required':
-        #         # 'Perks offered':
-        #         # 'About':
-        #         # 'this':
-        #         # 'job':
-        #         # 'Description':
-        #         # 'Job':
-        #         # 'link':
-        #
-        #         'text': quote.css('span.text::text').get(),
-        #         'author': quote.css('small.author::text').get(),
-        #         'tags': quote.css('div.tags a.tag::text').getall(),
-        #     }
+        yield {
+            'Job title': title.search(result[0]).group(1),
+
+            'Company': comp_name.search(result[0]).group(1),
+
+            'Company\'s logo Image Url': response.xpath(
+                '''normalize-space(//header[@class="job-details--header grid mb24 gs12 gsx sm:fd-column sm:ta-center"]
+                //img/@src)'''
+                ).extract(),
+            #logo.search(result[0]).group(1),
+
+            'Location': response.xpath(
+                '''normalize-space(//header[@class="job-details--header grid mb24 gs12 gsx sm:fd-column sm:ta-center"]
+                //span[@class="fc-black-500"]/text())'''
+                ).get(),
+              #str(locality.search(result[0]).group(1))
+               #             + ", " + str(region.search(result[0]).group(1))
+                #            + ", " + str(country.search(result[0]).group(1)),
+            'Skills Required': skills.search(result[0]).group(1),
+            'Perks offered': response.xpath(
+                '//section[contains(., "Benefits")]/ul//span/text()'
+                ).extract(),
+                #benefits.search(result[0]).group(1),
+
+            'About This Job': response.xpath(
+                '//section[contains(., "About this job")]//span/text()'
+                ).extract(),
+                #about.search(result[0]).group(1),
+
+            'Description': ''.join(response.xpath(
+                '//section[contains(., "Job description")]/div/ul//text()'
+                ).getall()),
+
+            'Job Link': response.url,
+        }
+
